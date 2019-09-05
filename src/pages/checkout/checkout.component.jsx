86ebdbd419b182944f0631d175b30ae605fda6bb
Lucas Guedes Barboza <lucasguedesbarboza@gmail.com>
2019-09-05 04:33:58 -0300

    Move directory and shop data into its own reducer, create respective selectors and update corresponding selectors and update components with new redux flow for directory/shop

--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -4,10 +4,7 @@ import { createStructuredSelector } from 'reselect';
 
 import CheckoutItem from '../../components/checkout-item/checkout-item.component';
 
-import { selectCartItems } from '../../redux/cart/cart-selectors';
-import { selectCartTotal } from '../../redux/cart/cart-selectors';
-
-import { Header } from '../../components/header/header.component';
+import { selectCartItems, selectCartTotal } from '../../redux/cart/cart-selectors';
 
 import './checkout.styles.scss';
 
